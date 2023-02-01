cd33980b7055d63cfae2d2c7217f42360a3ce4b6
Hasan Inam <inam2018225@gmail.com>
2023-02-01 11:22:22 +0300

    01.02.2022 Hv2

--- src/test/java/ilk_01022023.java
+++ src/test/java/ilk_01022023.java
@@ -1,5 +1,12 @@
 public class ilk_01022023 {
     public static void main(String[] args) {
         System.out.println("Hasan TestNg ==> 01.02.2023");
+
+
+
+
+
+
+        System.out.println("Hasan ikinci işlem");
     }
 }
