1b6b340bf780f56500f79b35c97f0985def3d248
Hasan Inam <inam2018225@gmail.com>
2023-02-01 11:16:00 +0300

    01.02.2023 Mv1

--- src/test/java/ilk_01022023.java
+++ src/test/java/ilk_01022023.java
@@ -1,5 +1,8 @@
 public class ilk_01022023 {
     public static void main(String[] args) {
         System.out.println("Hasan TestNg ==> 01.02.2023");
+
+
+        System.out.println("Master ilk işlem");
     }
 }
